1ff398505874b1cadefc2ab0747822e3afbc9892
therealmerengue@gmail.com <therealmerengue@gmail.com>
2018-04-28 15:44:46 +0200

    UI changes.

--- presentation/src/main/java/com/example/there/findclips/search/SearchFragment.kt
+++ presentation/src/main/java/com/example/there/findclips/search/SearchFragment.kt
@@ -2,9 +2,9 @@ package com.example.there.findclips.search
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.there.findclips.R
 import com.example.there.findclips.base.BaseVMFragment
 import com.example.there.findclips.util.app
@@ -20,6 +20,22 @@ class SearchFragment : BaseVMFragment<SearchViewModel>() {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.search_spinner_menu, menu)
+
+        val item = menu?.findItem(R.id.spinner)
+        val spinner = item?.actionView as? Spinner
+
+        val adapter = ArrayAdapter<String>(context, R.layout.spinner_item, arrayOf("Spotify", "Youtube"))
+        spinner?.adapter = adapter
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
     override fun initComponent() {
         activity?.app?.createSearchComponent()?.inject(this)
     }
@@ -31,4 +47,6 @@ class SearchFragment : BaseVMFragment<SearchViewModel>() {
     override fun releaseComponent() {
         activity?.app?.releaseSearchComponent()
     }
+
+
 }
