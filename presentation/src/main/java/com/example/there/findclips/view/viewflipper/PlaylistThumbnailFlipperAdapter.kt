4a50b66788071f0270c6d88e2af85b75eab369f5
therealmerengue@gmail.com <therealmerengue@gmail.com>
2018-09-26 22:05:40 +0200

    Setup for viewFlipper.

---
+++ presentation/src/main/java/com/example/there/findclips/view/viewflipper/PlaylistThumbnailFlipperAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.there.findclips.view.viewflipper
+
+import android.databinding.DataBindingUtil
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.example.there.findclips.R
+import com.example.there.findclips.databinding.ImageViewListItemBinding
+
+class PlaylistThumbnailFlipperAdapter(
+        private val thumbnailUrls: List<String>
+) : BaseAdapter() {
+
+    override fun getView(
+            position: Int,
+            view: View?,
+            parent: ViewGroup?
+    ): View {
+        val inflater = LayoutInflater.from(parent?.context)
+        val url = thumbnailUrls[position]
+        val holder: ViewHolder = if (view == null) {
+            val binding: ImageViewListItemBinding = DataBindingUtil.inflate(inflater, R.layout.image_view_list_item, parent, false)
+            ViewHolder(binding).apply { binding.url = url }
+            return binding.root
+        } else {
+            view.tag as ViewHolder
+        }
+
+        holder.binding.url = url
+        return view
+    }
+
+    override fun getItem(position: Int): Any? = null
+
+    override fun getItemId(position: Int): Long = 0
+
+    override fun getCount(): Int = thumbnailUrls.size
+
+    class ViewHolder(val binding: ImageViewListItemBinding)
+}
