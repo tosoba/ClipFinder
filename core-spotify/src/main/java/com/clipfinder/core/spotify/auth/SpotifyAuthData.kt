bb912b008520c3d39cc431a1cadaba5d57599515
Tomasz Osoba <tomasz.osoba@comarch.pl>
2020-09-23 16:35:02 +0200

    SpotifyAuthenticator setup.

---
+++ core-spotify/src/main/java/com/clipfinder/core/spotify/auth/SpotifyAuthData.kt
@@ -0,0 +1,5 @@
+package com.clipfinder.core.spotify.auth
+
+object SpotifyAuthData {
+    const val CLIENT_ID = "6dc5e6590b8b48c5bd73a64f6c206d8a"
+}
--- spotify-api/src/main/java/com/clipfinder/spotify/api/auth/SpotifyAuthenticator.kt
+++ spotify-api/src/main/java/com/clipfinder/spotify/api/auth/SpotifyAuthenticator.kt
@@ -1,4 +1,44 @@
 package com.clipfinder.spotify.api.auth
 
-class SpotifyAuthenticator {
+import com.clipfinder.core.spotify.auth.SpotifyAuthData
+import com.clipfinder.core.spotify.token.AccessTokenHolder
+import com.clipfinder.core.spotify.token.RefreshTokenHolder
+import com.clipfinder.spotify.api.endpoint.AuthEndpoints
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+
+class SpotifyAuthenticator(
+    private val accessTokenHolder: AccessTokenHolder,
+    private val refreshTokenHolder: RefreshTokenHolder,
+    private val auth: AuthEndpoints
+) : Authenticator {
+    override fun authenticate(route: Route?, response: Response): Request? {
+        val refreshTokenResponse = auth.refreshToken(
+            grantType = REFRESH_TOKEN_GRANT_TYPE,
+            refreshToken = requireNotNull(refreshTokenHolder.refreshToken),
+            clientId = SpotifyAuthData.CLIENT_ID
+        ).execute()
+
+        if (!refreshTokenResponse.isSuccessful) {
+            // AppAnalytics?
+            return null
+        }
+
+        val responseBody = requireNotNull(refreshTokenResponse.body()) {
+            "RefreshToken response body is null."
+        }
+
+        accessTokenHolder.token = responseBody.accessToken
+        refreshTokenHolder.refreshToken = responseBody.refreshToken
+
+        return response.request.newBuilder()
+            .header("Authorization", "Bearer ${responseBody.accessToken}")
+            .build()
+    }
+
+    companion object {
+        private const val REFRESH_TOKEN_GRANT_TYPE = "refresh_token"
+    }
 }
