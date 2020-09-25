1fb394b38e54bae59e1b204db9f89ffb48116a80
Tomasz Osoba <tomasz.osoba@comarch.pl>
2020-09-25 16:13:13 +0200

    HttpException on unsuccessful refreshTokenResponse.

--- spotify-api/src/main/java/com/clipfinder/spotify/api/auth/SpotifyAuthenticator.kt
+++ spotify-api/src/main/java/com/clipfinder/spotify/api/auth/SpotifyAuthenticator.kt
@@ -8,6 +8,7 @@ import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
+import retrofit2.HttpException
 
 class SpotifyAuthenticator(
     private val tokensHolder: SpotifyTokensHolder,
@@ -21,8 +22,7 @@ class SpotifyAuthenticator(
         ).execute()
 
         if (!refreshTokenResponse.isSuccessful) {
-            // AppAnalytics?
-            return null
+            throw HttpException(refreshTokenResponse)
         }
 
         val responseBody = requireNotNull(refreshTokenResponse.body()) {
